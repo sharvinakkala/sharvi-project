b492f5757731fc75cec22bec7d1b964e6197b094
sharvinakkala <nsharvireddy@gmail.com>
2021-06-29 13:44:46 -0500

    practice Xcode

---
+++ sharvi-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sharvi-project
+//
+//  Created by Sharvi Nakkala on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
